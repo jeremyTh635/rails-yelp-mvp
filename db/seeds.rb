4652227f93300d862bee5968b62e8f52cc95a2b6
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-02-05 17:39:37 +0000

    Modified or created files to create app

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+strawberry = { name: "The Strawberry Thief", address: "26 Broad St., Bristol BS1 2HG", phone_number: "0117 925 6925", category: "belgian" }
+mayflower = { name: "Mayflower", address: "1a, 5a Haymarket Walk, Bristol BS1 3LN", phone_number: "0117 925 0555", category: "chinese" }
+fujiyama = { name: "Fujiyama", address: "21 Regent St., Clifton, Bristol BS8 4HW", phone_number: "0117 907 7840", category: "japanese" }
+guingette = { name: "La Guingette", address: "243 Cheltenham Rd., Bristol BS6 5QP", phone_number: "0117 329 0600", category: "french" }
+san_carlo = { name: "San Carlo", address: "44 Corn St., Bristol BS1 1HQ", phone_number: "0117 922 6586", category: "italian" }
+little_french = { name: "Little French", address: "2 North View, Westbury Park, Bristol BS6 7QB", phone_number: "0117 970 6276", category: "french"}
+[strawberry, mayflower, fujiyama, guingette, san_carlo, little_french].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
